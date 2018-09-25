0f4e69428604415813442428e3359a20b6c0f161
virajvchavan <viraj@joshsoftware.com>
2018-09-25 13:08:58 +0530

    added all currencies

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,5 @@
 module DashboardHelper
   def currency_list
-    ['INR', 'USD']
+    ["CAD", "DKK", "AUD", "EUR", "HUF", "JPY", "HKD", "NOK", "ILS", "HRK", "ISK", "TRY", "MYR", "SGD", "CNY", "USD", "INR", "BGN", "IDR", "CHF", "SEK", "CZK", "BRL", "PHP", "MXN", "ZAR", "PLN", "NZD", "KRW", "RON", "THB", "RUB", "GBP"]
   end
 end
